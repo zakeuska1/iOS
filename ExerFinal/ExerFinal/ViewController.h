4a490e9a6d794090b67521bf2d88d0958a07d0c0
ALUNO <aluno@hc5mac14.local>
2016-08-10 20:19:02 -0300

    criação do projeto

---
+++ ExerFinal/ExerFinal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ExerFinal
+//
+//  Created by ALUNO on 10/08/16.
+//  Copyright © 2016 IESB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ExerFinal/ExerFinal/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ExerFinal
+//
+//  Created by ALUNO on 10/08/16.
+//  Copyright © 2016 IESB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
