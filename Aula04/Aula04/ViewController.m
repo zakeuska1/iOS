ec61e227dcfcafe7cbb55edfe43948571daba428
ALUNO <aluno@hc5mac14.local>
2016-08-17 22:13:15 -0300

    inclusão de imagens e gestos

---
+++ Aula04/Aula04/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Aula04
+//
+//  Created by ALUNO on 17/08/16.
+//  Copyright © 2016 IESB. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
