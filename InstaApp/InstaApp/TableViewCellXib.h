2fda837e08f93c5023ed9ce4bbc2b5fa0f84cbaf
ALUNO <aluno@hc5mac14.local>
2016-10-26 19:32:00 -0200

    passo 0

---
+++ InstaApp/InstaApp/TableViewCellXib.h
@@ -0,0 +1,13 @@
+//
+//  TableViewCellXib.h
+//  InstaApp
+//
+//  Created by ALUNO on 26/10/16.
+//  Copyright © 2016 IESB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCellXib : UITableViewCell
+
+@end
---
+++ InstaApp/InstaApp/TableViewCellXib.m
@@ -0,0 +1,24 @@
+//
+//  TableViewCellXib.m
+//  InstaApp
+//
+//  Created by ALUNO on 26/10/16.
+//  Copyright © 2016 IESB. All rights reserved.
+//
+
+#import "TableViewCellXib.h"
+
+@implementation TableViewCellXib
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
