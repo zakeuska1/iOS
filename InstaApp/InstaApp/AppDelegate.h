9f3cc14348a8891cbe8148c10b32085f1776fce4
ALUNO <aluno@hc5mac14.local>
2016-10-26 19:28:16 -0200

    passo 0

---
+++ InstaApp/InstaApp/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  InstaApp
+//
+//  Created by ALUNO on 26/10/16.
+//  Copyright © 2016 IESB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ InstaApp/InstaApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstaApp
+//
+//  Created by ALUNO on 26/10/16.
+//  Copyright © 2016 IESB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
