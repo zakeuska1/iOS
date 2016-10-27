41cdd2602bdd5885fa713fbf3cf6f0dac3513f40
ALUNO <aluno@hc5mac14.local>
2016-10-26 22:14:36 -0200

    Passo 0

---
+++ FaceApp/FaceApp/TableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TableViewCell.h
+//  FaceApp
+//
+//  Created by ALUNO on 26/10/16.
+//  Copyright © 2016 IESB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCell : UITableViewCell
+
+@end
---
+++ FaceApp/FaceApp/TableViewCell.m
@@ -0,0 +1,31 @@
+//
+//  TableViewCell.m
+//  FaceApp
+//
+//  Created by ALUNO on 26/10/16.
+//  Copyright © 2016 IESB. All rights reserved.
+//
+
+#import "TableViewCell.h"
+
+@interface TableViewCell ()
+@property (weak, nonatomic) IBOutlet UIView *post;
+
+@end
+
+@implementation TableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    
+    [self.post.layer setBorderColor:[[UIColor colorWithWhite:0.3 alpha:1] CGColor]]
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
